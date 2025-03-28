283bf41ee83d62d2956c10774c42a18cfd137393
romajet <romanusmanov03@gmail.com>
2025-03-28 22:34:08 +0500

    first

---
+++ app/src/main/java/com/example/susumapapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.susumapapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webview)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+        webView.settings.allowFileAccessFromFileURLs = true
+        webView.settings.allowUniversalAccessFromFileURLs = true
+        webView.settings.javaScriptCanOpenWindowsAutomatically = true
+
+        WebView.setWebContentsDebuggingEnabled(true)
+
+        webView.loadUrl("file:///android_asset/index.html")
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                url: String
+            ): Boolean {
+                view?.loadUrl(url)
+                return false
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        val webView: WebView = findViewById(R.id.webview)
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
