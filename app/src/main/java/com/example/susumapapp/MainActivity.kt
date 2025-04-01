44fc1e211f9f05a37fd41844f807ce38f5a2374c
romajet <romanusmanov03@gmail.com>
2025-04-01 12:34:36 +0500

    third

--- app/src/main/java/com/example/susumapapp/MainActivity.kt
+++ app/src/main/java/com/example/susumapapp/MainActivity.kt
@@ -2,27 +2,24 @@ package com.example.susumapapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.webkit.JavascriptInterface
-import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var webView: WebView
-    private var isOnBuildingList = false
-
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webview)
 
-        webView = findViewById(R.id.webview)
         webView.settings.javaScriptEnabled = true
         webView.settings.allowFileAccessFromFileURLs = true
         webView.settings.allowUniversalAccessFromFileURLs = true
 
         WebView.setWebContentsDebuggingEnabled(true)
+
+        webView.loadUrl("file:///android_asset/index.html")
 
         webView.webViewClient = object : WebViewClient() {
             override fun shouldOverrideUrlLoading(
@@ -33,27 +30,14 @@ class MainActivity : AppCompatActivity() {
                 return false
             }
         }
-
-        webView.webChromeClient = WebChromeClient()
-        webView.addJavascriptInterface(WebAppInterface(), "Android")
-
-        webView.loadUrl("file:///android_asset/index.html")
     }
 
     override fun onBackPressed() {
-        if (isOnBuildingList) {
-            finish()
-        } else if (webView.canGoBack()) {
+        val webView: WebView = findViewById(R.id.webview)
+        if (webView.canGoBack()) {
             webView.goBack()
         } else {
             super.onBackPressed()
-        }
-    }
-
-    inner class WebAppInterface {
-        @JavascriptInterface
-        fun updatePage(isBuildingList: Boolean) {
-            isOnBuildingList = isBuildingList
         }
     }
 }
